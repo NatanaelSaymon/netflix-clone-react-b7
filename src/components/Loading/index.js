1683d41abc7eee775a57e79cfccd40204a941483
Natanael Saymon <saymoneo@gmail.com>
2021-09-02 17:30:23 -0300

    Criando componente Loading

---
+++ src/components/Loading/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="" />
+    </div>
+  )
+}
+
+export default Loading
