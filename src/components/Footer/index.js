d9e155bedd43a3340c260e3227a90d91092255ac
Natanael Saymon <saymoneo@gmail.com>
2021-09-02 17:29:41 -0300

    Criando componente Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer">
+        Feito com <span role="img" aria-label="coração">❤️</span> por Natanael Saymon <br/>
+        Direitos imagem para Netflix <br/>
+        Dados pegos do site Themoivedb.org
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
