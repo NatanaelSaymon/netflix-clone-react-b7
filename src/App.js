89f755cdb9c918f1d088e15274bdbe12aceff49f
Natanael Saymon <saymoneo@gmail.com>
2021-09-02 17:30:39 -0300

    Finalizando o projeto

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 import Tmdb from './Tmdb'
 import MovieRow from './components/MovieRow'
 import FeaturedMovie from './components/FeaturedMovie'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Loading from './components/Loading'
 import './App.css'
 
 
@@ -11,6 +14,8 @@ const Default = () => {
   const [moveList, setMovieList] = useState([])
 
   const [featuredData, setfeaturedData] = useState(null)
+
+  const [scrollActive, setScrollActive] = useState(false)
 
   //Quando a tela for carregada, vai executar o codigo inserido
   useEffect(() => {
@@ -24,26 +29,48 @@ const Default = () => {
       let originals = list.filter(i=>i.slug === 'originals')
       let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
       let chosen = originals[0].items.results[randomChosen]
-
-      console.log(chosen)
-      
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+      setfeaturedData(chosenInfo)
     }
 
     loadAll()
   }, [])
 
+
+  //Adicioando evento de monitoramento do scroll da pagina
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10){
+        setScrollActive(true)
+      } else {
+        setScrollActive(false)
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener)
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, [])
+
   return(
     <div className="page">
 
-      {featuredData && 
-        <FeaturedMovie item={featuredData}/>
-      }
+      <Header scroll={scrollActive}/>
+
+      {featuredData && <FeaturedMovie item={featuredData}/>}
 
       <section className="lists">
         { moveList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
         )) }
       </section>
+
+      <Footer />
+      
+      { moveList.length <= 0 && <Loading /> }
+
     </div>
   )
 }
