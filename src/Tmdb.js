c13d7538036824bb9e236a9b40af3c75cb89e883
Natanael Saymon <nsaymonsouza@gmail.com>
2021-09-02 23:45:51 -0300

    Melhorando a escrita dos titulos

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -27,6 +27,7 @@ const basicFetch = async (endpoint) => {
 
 
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   getHomeList: async () => {
     return [
@@ -37,7 +38,7 @@ export default {
       },
       {
         slug: 'trending',
-        title: 'Recomendados para voce',
+        title: 'Recomendados para você',
         items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
       },
       {
@@ -47,27 +48,27 @@ export default {
       },
       {
         slug: 'action',
-        title: 'ação',
+        title: 'Ação',
         items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'comedy',
-        title: 'comedia',
+        title: 'Comedia',
         items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'Horror',
-        title: 'terror',
+        title: 'Terror',
         items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'romance',
-        title: 'romance',
+        title: 'Romance',
         items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'documentary',
-        title: 'documentarios',
+        title: 'Documentarios',
         items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
       },
     ]
